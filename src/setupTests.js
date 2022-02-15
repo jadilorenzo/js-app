0a8e57a1c5f50a633ee3eee5fab5986eaf6d4b8b
Jacob Di Lorenzo <46056370+jadilorenzo@users.noreply.github.com>
2022-02-15 16:31:20 -0500

    Setup tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
